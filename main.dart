047f90ff21e37c4579396ae2792cd4d708ab3963
Thinagaran.T <78643737+Thina007@users.noreply.github.com>
2021-08-08 10:59:40 +0530

    Calculater app

---
+++ main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: calculater(),
+    ));
+
+class calculater extends StatefulWidget {
+  const calculater({Key? key}) : super(key: key);
+
+  @override
+  _calculaterState createState() => _calculaterState();
+}
+
+class _calculaterState extends State<calculater> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculater'),
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(5),
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              SizedBox(
+                height: 270,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  text,
+                  style: TextStyle(
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepOrange,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  numbers("9"),
+                  numbers("8"),
+                  numbers("7"),
+                  numbers("+")
+                ],
+              ),
+              Row(
+                children: [
+                  numbers("6"),
+                  numbers("5"),
+                  numbers("4"),
+                  numbers("-")
+                ],
+              ),
+              Row(
+                children: [
+                  numbers("3"),
+                  numbers("2"),
+                  numbers("1"),
+                  numbers("*")
+                ],
+              ),
+              Row(
+                children: [
+                  numbers("C"),
+                  numbers("0"),
+                  numbers("="),
+                  numbers("/")
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget numbers(String num) {
+    return Expanded(
+      child: OutlineButton(
+        onPressed: () {
+          btnclicked(num);
+        },
+        child: Text(
+          "$num",
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            height: 3,
+          ),
+        ),
+      ),
+    );
+  }
+
+  String res = "", text = "", thina = '', garan = '';
+  int first = 0, second = 0, n = 0;
+
+  String opp = '';
+  void btnclicked(String val) {
+    if (val == 'C') {
+      res = '';
+      thina = '';
+    } else if (val == '+' || val == '-' || val == '*' || val == '/') {
+      first = int.parse(text);
+      opp = val;
+      thina = thina + val;
+      res = '';
+    } else if (val == '=') {
+      n = 1;
+      second = int.parse(text);
+      thina = thina + val;
+      if (opp == '+') {
+        res = (first + second).toString();
+      }
+      if (opp == '-') {
+        res = (first - second).toString();
+      }
+      if (opp == '*') {
+        res = (first * second).toString();
+      }
+      if (opp == '/') {
+        res = (first ~/ second).toString();
+      }
+    } else {
+      res = text + val;
+      thina = thina + val;
+    }
+
+    setState(() {
+      text = res;
+      garan = thina;
+    });
+  }
+}
